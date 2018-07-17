2a0995b9bb8d7ee62112fc28ab906a0a7f7cd60c
Aleks Khomovych <oleksand.khomovych@gmail.com>
2018-07-17 09:43:02 +0300

    updated

--- core/actions/actions_api.js
+++ core/actions/actions_api.js
@@ -19,7 +19,7 @@ class ActionsAPI {
   		}		
 	}
 
-	async get_sity_ref(api_url, body_request) {
+	async get_city_ref(api_url, body_request) {
 		let responce = await this.post_request(api_url, body_request);
 		try {
 			return responce['data'][0]['Ref'];
@@ -35,6 +35,18 @@ class ActionsAPI {
   	async get_waybill_data(api_url, body_request) {
   		return await this.post_request(api_url, body_request);
   	}
+
+  	async fill_waybill_json(waybill_json, test_data, city_ref, city_recipient_ref) {
+  		waybill_json['methodProperties']['CitySender'] = city_ref;
+  		waybill_json['methodProperties']['CityRecipient'] = city_recipient_ref;
+  		waybill_json['methodProperties']['Weight'] = test_data['weight'];
+  	  	waybill_json['methodProperties']['ServiceType'] = test_data['service_type'];
+  	  	waybill_json['methodProperties']['Cost'] = test_data['cost'];
+  		waybill_json['methodProperties']['CargoType'] = test_data['cargo_type'];
+  		waybill_json['methodProperties']['SeatsAmount'] = test_data['seats_amount'];
+  		return waybill_json;
+  	}
+
 };
 
 module.exports = ActionsAPI;
--- test/test_api.js
+++ test/test_api.js
@@ -11,40 +11,31 @@ let actionsAPI = new ActionsAPI(chai, chaiHttp);
 
 let apiData = require('../core/utils/api_data_reader.js');
 
-describe('', async function() {
+describe('/POST API Nova Poshta', async function() {
 	this.timeout(apiData.get_timeout());
 
-	it('/POST API Nova Poshta', async function() {
-		let sity_ref = await actionsAPI.get_sity_ref(apiData.get_api_url(), apiData.get_setelements_data());
+	it('As a developer I want to get list of nearest departments through the API ', async function() {
+		let city_ref = await actionsAPI.get_city_ref(apiData.get_api_url(), apiData.get_setelements_data());
 
 		let warehouses_json = apiData.get_warehouses_data();
-		warehouses_json['methodProperties']['SettlementRef'] = sity_ref;
+		warehouses_json['methodProperties']['SettlementRef'] = city_ref;
 
 		let warehouses_data = await actionsAPI.get_warehouses_data(apiData.get_api_url(), warehouses_json);
 
 		warehouses_data.should.have.property('data');
 		warehouses_data.should.have.property('success').eql(true);
-      });      
+  });      
 
-  describe('Nova Poshta waybill', async function() {
-    forEach(apiData.get_waybill_data())
+  forEach(apiData.get_waybill_data())
     .it('As a developer I want to create express waybill through the API',
   	 async function(test_data) {
-  		  let city_ref = await actionsAPI.get_sity_ref(apiData.get_api_url(), apiData.get_setelements_data());
-  		  let city_recipient_ref = await actionsAPI.get_sity_ref(apiData.get_api_url(), apiData.get_setelements_recipient(test_data['city_recipient']));
+  		  let city_ref = await actionsAPI.get_city_ref(apiData.get_api_url(), apiData.get_setelements_data());
+  		  let city_recipient_ref = await actionsAPI.get_city_ref(apiData.get_api_url(), apiData.get_setelements_recipient(test_data['city_recipient']));
 
   		  let waybill_json = apiData.get_document_price();
-  		  waybill_json['methodProperties']['CitySender'] = city_ref;
-  		  waybill_json['methodProperties']['CityRecipient'] = city_recipient_ref;
-  		  waybill_json['methodProperties']['Weight'] = test_data['weight'];
-  		  waybill_json['methodProperties']['ServiceType'] = test_data['service_type'];
-  		  waybill_json['methodProperties']['Cost'] = test_data['cost'];
-  		  waybill_json['methodProperties']['CargoType'] = test_data['cargo_type'];
-  		  waybill_json['methodProperties']['SeatsAmount'] = test_data['seats_amount'];
+        let waybill = await actionsAPI.fill_waybill_json(waybill_json, test_data, city_ref, city_recipient_ref);
+  		  let waybill_data = await actionsAPI.get_waybill_data(apiData.get_api_url(), waybill);
 
-  		  let waybill_data = await actionsAPI.get_waybill_data(apiData.get_api_url(), waybill_json);
-
-        waybill_data.should.have.property('success').eql(true);
+        waybill_data.should.have.property('success').eql(test_data['expected']);
 	 });
-  });
 });
